83f2a9c2e28ea2e77be4c1bc18e4d32c311b8f96
Gabe Velasquez <gvelasquez@trackforce.com>
2019-01-08 14:39:19 -0800

    passing components into other components using props.children

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import faker from 'faker';
+import React from "react";
 
-const CommentDetail = (props) => {
-    return (
-        <div className="comment">
-            <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()} />
-            </a>
-            <div className="content">
-                <a href="/" className="author">
-                    {props.author}
-                </a>
-                <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
-                </div>
-                <div className="text">Nice blog post!</div>
-            </div>
+const CommentDetail = props => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={props.avatarURL} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {props.author}
+        </a>
+        <div className="metadata">
+          <span className="date">{props.timeAgo}</span>
         </div>
-    );
+        <div className="text">{props.commentText}</div>
+      </div>
+    </div>
+  );
 };
 
 export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <CommentDetail author="Sam"/>
-            <CommentDetail author="Alex"/>
-            <CommentDetail author="Jane"/>
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning</h4>
+          Are you sure you want to do this?
         </div>
-    );
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45pm"
+          avatarURL={faker.image.avatar()}
+          commentText={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00am"
+          avatarURL={faker.image.avatar()}
+          commentText={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00pm"
+          avatarURL={faker.image.avatar()}
+          commentText={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
